f4f54de1d57a0b496c34d986ef6f310abe7ddf41
rowland henshaw <rowland.henshaw@gmail.com>
2018-07-04 21:09:57 +0100

    added stylesheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,57 @@ export class Hero {
   name: string;
 }
 
+const appStyles =[`
+ .selected {
+   background-color: #CFD8DC !important;
+   color: white;
+ }
+ .heroes {
+   margin: 0 0 2em 0;
+   list-style-type: none;
+   padding: 0;
+   width: 15em;
+ }
+ .heroes li {
+   cursor: pointer;
+   position: relative;
+   left: 0;
+   background-color: #EEE;
+   margin: .5em;
+   padding: .3em 0;
+   height: 1.6em;
+   border-radius: 4px;
+ }
+ .heroes li.selected:hover {
+   background-color: #BBD8DC !important;
+   color: black;
+ }
+ .heroes li:hover {
+   color: #607D8B;
+   background-color: #DDD;
+   left: .1em;
+ }
+ .heroes .text {
+   position: relative;
+   top: -3px;
+ }
+ .heroes .badge {
+   display: inline-block;
+   font-size: small;
+   color: white;
+   padding: 0.8em 0.7em 0 0.7em;
+   background-color: #607D8B;
+   line-height: 1em;
+   position: relative;
+   left: -1px;
+   top: -4px;
+   height: 1.8em;
+   margin-right: .8em;
+   border-radius: 4px 0 0 4px;
+ }
+`]
+
+
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -22,15 +73,20 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
       <h1>{{title}}</h1>
-      <h2>{{hero.name}} details</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
       <div>
         <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
+        <input [(ngModel)]="selectedHero.name" placeholder="name">
+      </div>
       </div>
       <h2>My Heroes</h2>
-      <ul>
-        <li *ngFor="let hero of heroes">
+      <ul class="heroes">
+        <li *ngFor="let hero of heroes"
+          (click)=onSelect(hero)
+          [class.selected]="hero === selectedHero"
+        >
           <span class="badge">
             {{hero.id}}
           </span>
@@ -38,6 +94,7 @@ const HEROES: Hero[] = [
         </li>
       </ul>
       `,
+      styles: appStyles
 })
 export class AppComponent  {
   title = 'Tour of heroes';
@@ -46,5 +103,11 @@ export class AppComponent  {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
+  };
+  selectedHero: Hero;
+  onSelect(hero :Hero): void {
+    this.selectedHero = hero
   }
  }
+
+ 
